5256d1daefa55d4e7bfa55884bf335a34805f3ea
Thomas Polverelli <thomas.polverelli@epita.fr>
2025-06-27 20:08:33 +0200

    feat(Searchbar): New SearchBar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,96 +1,12 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 import { MovieListComponent } from './movie-list/movie-list.component';
-import { ApiCallService, Genre } from './api-call.service';
-import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, FormsModule, MovieListComponent, CommonModule],
+    imports: [MovieListComponent, SearchBarComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-    @ViewChild(MovieListComponent) movieList!: MovieListComponent;
-    searchText: string = '';
-    isFirstClick = true;
-    currentYear = new Date().getFullYear();
-    years: number[] = [];
-    genres: Genre[] = [];
-    selectedAlphabetic: string = 'popularity.desc';
-    selectedYear: string = "";
-    selectedGenre: string = "";
-
-    constructor(private api_call: ApiCallService) {}
-
-    onSearch() {
-        const filtersElement = document.getElementById('filters');
-        if (filtersElement && !filtersElement.classList.contains('d-none')) {
-            const alphabeticSelect = document.getElementById("alphabeticSelect") as HTMLSelectElement | null;
-            if (alphabeticSelect) {
-                this.selectedAlphabetic = alphabeticSelect.value;
-            }
-
-            const yearSelect = document.getElementById("yearSelect") as HTMLSelectElement | null;
-            if (yearSelect) {
-                this.selectedYear = yearSelect.value;
-            }
-
-            const genreSelect = document.getElementById("genre") as HTMLSelectElement | null;
-            if (genreSelect) {
-                this.selectedGenre = genreSelect.value;
-            }   
-        } else {
-            this.selectedAlphabetic = 'popularity.desc';
-            this.selectedYear = '';
-            this.selectedGenre = '';
-        }
-
-        this.movieList.search(this.searchText, this.selectedAlphabetic, this.selectedYear, this.selectedGenre);
-    }
-
-    displayFilters() {
-        const filtersElement = document.getElementById('filters');
-        if (filtersElement) {
-            filtersElement.classList.remove('d-none');
-        }
-    }
-
-    hideFilters() {
-        const filtersElement = document.getElementById('filters');
-        if (filtersElement) {
-            filtersElement.classList.add('d-none');
-        }
-    }
-
-    ngOnInit() {
-        console.log('ngOnInit running');
-        this.hideFilters();
-        for (let year = this.currentYear; year >= 1900; year--) {
-            this.years.push(year);
-        }
-
-        this.api_call.getMovieGenres().subscribe((response) => {
-            this.genres = response.genres;
-        });
-    }
-
-    handleClick() {
-        if (this.isFirstClick) {
-            this.displayFilters();
-            const filtersElement = document.getElementById('btn_filters');
-            if (filtersElement) {
-                filtersElement.innerText = 'Filters -';
-            }
-        } else {
-            this.hideFilters();
-            const filtersElement = document.getElementById('btn_filters');
-            if (filtersElement) {
-                filtersElement.innerText = 'Filters +';
-            }
-        }
-        this.isFirstClick = !this.isFirstClick;
-    }
-}
+export class AppComponent {}
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiCallService, Genre } from '../api-call.service';
+
+@Component({
+    selector: 'app-search-bar',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.css'],
+})
+export class SearchBarComponent implements OnInit {
+    @Output() search = new EventEmitter<{
+        query: string;
+        alphabetic: string;
+        year: string;
+        genre: string;
+    }>();
+
+    searchText: string = '';
+    isFirstClick: boolean = true;
+    currentYear = new Date().getFullYear();
+    years: number[] = [];
+    genres: Genre[] = [];
+    selectedAlphabetic: string = 'original_title.asc';
+    selectedYear: string = '';
+    selectedGenre: string = '';
+
+    constructor(private apiCall: ApiCallService) {}
+
+    ngOnInit(): void {
+        for (let year = this.currentYear; year >= 1900; year--) {
+            this.years.push(year);
+        }
+        this.apiCall.getMovieGenres().subscribe((response) => {
+            this.genres = response.genres;
+        });
+    }
+
+    onSearch(): void {
+        this.search.emit({
+            query: this.searchText,
+            alphabetic: this.selectedAlphabetic,
+            year: this.selectedYear,
+            genre: this.selectedGenre,
+        });
+    }
+
+    toggleFilters(): void {
+        this.isFirstClick = !this.isFirstClick;
+    }
+}
